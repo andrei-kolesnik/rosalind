5f09e433579ba8984ceca80b19c15cfb1e54f844
andrei-kolesnik <andrei.kolesnik@gmail.com>
2018-10-18 16:13:54 -0700

    FIB

---
+++ FIB/FIB.cpp
@@ -0,0 +1,33 @@
+//http://rosalind.info/problems/fib/
+//Samples
+//5 3 -> 19
+//28 5 -> 662854323131
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+unordered_map<int, __int64> fibs;
+
+__int64 fib(int n, int k)
+{
+	auto found = fibs.find(n);
+	if (found != fibs.end())
+		return found->second;
+
+	__int64 result = 0;
+	if (n < 3) result = 1;
+	else
+	{
+		result = fib(n - 2, k) * k + fib(n - 1, k);
+	}
+	fibs[n] = result;
+	return result;
+}
+
+int main()
+{
+//	cout << fib(5, 3) << endl;
+	cout << fib(28, 5) << endl; //662854323131
+	//for (auto it = fibs.begin(); it != fibs.end(); it++)
+	//	cout << it->first << "=" << it->second << endl;
+	getchar();
+}
--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 * SortedDictionary
 ### [RNA](RNA): Transcribing DNA into RNA [C#.NET]
 * String.Replace
-### [REVC](REVC): Complementing a Strand of DNA [C#.NET]
+### [REVC](REVC): Complementing a Strand of DNA [STL]
 * ifstream/ofstream
 * stringstream
+* unordered_map
+### [FIB](FIB): Rabbits and Recurrence Relations [STL]
+* recursion
 * unordered_map
