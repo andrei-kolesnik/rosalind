d1bc4e44b3a7b8b8a8d32c2743b442f6ce28cf57
andrei-kolesnik <andrei.kolesnik@gmail.com>
2018-10-19 17:12:28 -0700

    SUBS

--- README.md
+++ README.md
@@ -4,15 +4,18 @@
 * SortedDictionary
 ### [RNA](RNA): [Transcribing DNA into RNA](http://rosalind.info/problems/rna/) [C#.NET]
 * String.Replace
-### [REVC](REVC): [Complementing a Strand of DNA](http://rosalind.info/problems/revc/) [STL]
+### [REVC](REVC): [Complementing a Strand of DNA](http://rosalind.info/problems/revc/) [C++ STL]
 * ifstream/ofstream
 * stringstream
 * unordered_map
-### [FIB](FIB): [Rabbits and Recurrence Relations](http://rosalind.info/problems/fib/) [STL]
+### [FIB](FIB): [Rabbits and Recurrence Relations](http://rosalind.info/problems/fib/) [C++ STL]
 * recursion
 * unordered_map
 ### [GC](GC): [Computing GC Content](http://rosalind.info/problems/gc/) [C#.NET]
-* struct
 * File.ReadAllLines
 * Dictionary
-* KeyValuePair
+* Linq
+### [SUBS](SUBS): [Finding a Motif in DNA](http://rosalind.info/problems/subs/) [C#.NET]
+* extension methods (C# 3.0)
+* IEnumerable
+* yield
---
+++ SUBS/SUBS.cs
@@ -0,0 +1,38 @@
+﻿//http://rosalind.info/problems/subs/
+//Sample Dataset
+//GATATATGCATATACTT
+//ATAT
+//Sample Output
+//2 4 10
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Text.RegularExpressions;
+
+namespace Rosalind
+{
+    internal static class SUBS
+    {
+        public static IEnumerable<int> Subs(this string source, string motif)
+        {
+            //foreach (Match match in Regex.Matches(source, Regex.Escape(motif))) //does not find overlapping results
+            //    yield return match.Index+1;
+            int result = 0;
+            while (result < source.Length)
+            {
+                result = source.IndexOf(motif, result) + 1;
+                if (result == 0) break;
+                yield return result; 
+            }
+        }
+        static void Main(string[] args)
+        {
+            string dna = "GATATATGCATATACTT", motif = "ATAT";
+            foreach (int it in dna.Subs(motif))
+                System.Console.Write("{0} ", it);
+            System.Console.ReadKey();
+        }
+    }
+}
